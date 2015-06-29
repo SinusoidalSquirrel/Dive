ca9ff838288dc9a92856d0cc3c7a076ebc1d95e6
Bryan Liu <bsliu17@gmail.com>
2015-06-29 10:15:59 -0700

    redis server url

--- server/db/database.js
+++ server/db/database.js
@@ -1,13 +1,13 @@
 var redis = require('redis');
 
-// By default, redis.createClient() will use 127.0.0.1 and 6379 as the hostname and port respectively. 
+// By default, redis.createClient() will use 127.0.0.1 and 6379 as the hostname and port respectively.
 // var client = redis.createClient(port, host);
 
 console.log("process.env.REDISTOGO_URL: ", process.env);
 if (process.env.REDISTOGO_URL) {
   // TODO: redistogo connection
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  var client = redis.createClient(rtg.port, rtg.hostname);
+  var client = redis.createClient(rtg.port, rtg.href);
   console.log('rtg:', rtg);
 
   client.auth(rtg.auth.split(":")[1]);
